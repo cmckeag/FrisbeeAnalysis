a71fb5727a8dde5f25c78857f8f8bad88eded8d6
Christopher McKeag <christophermckeag@Christophers-MacBook-Pro.local>
2018-04-25 09:00:27 -0700

    Saving for later

--- R/manip.R
+++ R/manip.R
@@ -2,6 +2,8 @@
 # ultianalytics.
 
 get.points = function(raw) {
+  # Change from inputting raw data to inputting filename, then
+  # work the data myself
   desired = c("Event.Type",
               "Player.0","Player.1","Player.2","Player.3","Player.4",
               "Player.5","Player.6","Player.7")
@@ -11,5 +13,5 @@ get.points = function(raw) {
   players = players[order(players)]
   new.dat = data.frame(matrix(vector(), nrow = 0, ncol = 1 + length(players)))
   colnames(new.dat) = c("Scored",players)
-  
+  # See https://stackoverflow.com/questions/25347739/r-convert-factor-column-to-multiple-boolean-columns
 }
