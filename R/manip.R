910c8ba6d1415bc80f2243c4fb58202691ff0d4f
Christopher McKeag <christophermckeag@Christophers-MacBook-Pro.local>
2018-04-25 08:32:46 -0700

    Added "data" folder to .gitignore
    Working on getpoints() function in manip

--- R/manip.R
+++ R/manip.R
@@ -2,5 +2,14 @@
 # ultianalytics.
 
 get.points = function(raw) {
-  return(FALSE)
+  desired = c("Event.Type",
+              "Player.0","Player.1","Player.2","Player.3","Player.4",
+              "Player.5","Player.6","Player.7")
+  working.data = raw[raw$Action == "Goal",desired]
+  players = unique(unlist(working.data[,desired[-1]]))
+  players = as.character(players[!is.na(players)])
+  players = players[order(players)]
+  new.dat = data.frame(matrix(vector(), nrow = 0, ncol = 1 + length(players)))
+  colnames(new.dat) = c("Scored",players)
+  
 }
