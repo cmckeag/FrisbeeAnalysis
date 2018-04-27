657d2698ee70560eeffd855f9d6687dd22a329c5
Christopher McKeag <christophermckeag@Christophers-MacBook-Pro.local>
2018-04-27 16:20:20 -0700

    New extract.data() function

--- R/manip.R
+++ R/manip.R
@@ -44,8 +44,7 @@ get.dur = function(filename) {
   desired = c("Point.Elapsed.Seconds",
               "Player.0","Player.1","Player.2","Player.3","Player.4",
               "Player.5","Player.6","Player.7")
-  dur.data = raw[!duplicated(raw[,desired]),desired]
-  dur.data = dur.data[which(dur.data[,"Player.0"] != ""),]
+  dur.data = raw[raw$Action == "Goal",desired]
   players = get.all.players(raw)
   new.dat = data.frame(Duration = dur.data[,1], t(as.matrix(apply(dur.data[,desired[-1]],1,function(x) players %in% x))))
   colnames(new.dat) = c("Duration",players)
@@ -64,4 +63,72 @@ get.all.players = function(raw) {
   players = players[!is.na(players)]
   players = players[order(players)]
   return(players)
+}
+
+extract.data = function(filename) {
+  if (grepl(".csv",filename)) {
+    filename = gsub(".csv","",filename)
+  }
+  raw = get.raw(filename)
+  
+  raw2 = ignore.cessations(raw)
+  
+  passes = count.passes(raw2)
+  possessions = count.possessions(raw2)
+  
+  raw3 = raw2[raw2$Action == "Goal",]
+  
+  outcome = (raw3$Event.Type == "Offense")
+  
+  players = get.all.players(raw3)
+  desired = c("Player.0","Player.1","Player.2","Player.3","Player.4",
+              "Player.5","Player.6","Player.7")
+  
+  new.dat = data.frame(Scored = outcome,
+                       Duration = raw3$Point.Elapsed.Seconds,
+                       Passes = passes,
+                       Possessions = possessions,
+                       Line = raw3$Line,
+                       t(as.matrix(apply(raw3[,desired],1,function(x) players %in% x))))
+  colnames(new.dat) = c("Scored","Duration","Passes","Possessions","Line",players)
+  
+  write.csv(new.dat, file = paste("out/", filename, ".csv", sep = ""), row.names=FALSE)
+  return(new.dat)
+}
+
+ignore.cessations = function(raw) {
+  ## Helper function
+  ## Ignore anything that happens immediately before
+  ## a cessation. Only applies to AUDL games, where it is
+  ## possible for a point to end without a goal.
+  cess.index = which(raw$Event.Type == "Cessation")
+  goal.index = which(raw$Action == "Goal")
+  ignore.index = numeric()
+  if (length(cess.index) > 0) {
+    for (i in cess.index) {
+      last = goal.index[max(which(goal.index < i))]
+      ignore.index = c(ignore.index, (last+1):i)
+    }
+  }
+  raw[-ignore.index,]
+}
+
+count.passes = function(raw) {
+  # Helper function
+  # Counts the total number of passes completed (including goal if scored)
+  # in a point
+  goal.index = which(raw$Action == "Goal")
+  groups = cut(seq(1,nrow(raw)), breaks = c(0,goal.index), labels = FALSE)
+  pass.actions = raw$Action == "Catch" | 
+                (raw$Action == "Goal" & raw$Event.Type == "Offense")
+  as.vector(aggregate(pass.actions, by = list(groups), FUN = sum)$x)
+}
+
+count.possessions = function(raw) {
+  goal.index = which(raw$Action == "Goal")
+  groups = cut(seq(1,nrow(raw)), breaks = c(0,goal.index), labels = FALSE)
+  poss.actions = raw$Action == "Drop" | 
+                (raw$Action == "Throwaway" & raw$Event.Type == "Offense") | 
+                (raw$Action == "Goal" & raw$Event.Type == "Offense")
+  as.vector(aggregate(poss.actions, by = list(groups), FUN = sum)$x)
 }
